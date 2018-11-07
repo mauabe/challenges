e74296de24d4fa851b0927b25e081be59ef0f444
Mauricio Feldman-Abe <mauabe@gmx.com>
2018-11-06 17:35:44 -0800

    fixed binary search pointers

--- BinarySearch.js
+++ BinarySearch.js
@@ -1,21 +1,19 @@
 function binarySearch(array, target) { 
-	
-  const helper = function(array, target, pointerLow, pointerHigh){
-		pointerLow = 0;
-		pointerHigh = array.length-1;
-		let middle = Math.floor((pointerLow + pointerHigh)/2);
-		while (pointerLow <= pointerHigh){
-			if (target === array[middle]){return middle;}
-			else if	(target < array[middle]){
-				pointerHigh = middle - 1;
-			}
-			else {
-				pointerLow = middle + 1;
-			}
-		}
-		return -1;
-	}
-	
-	return helper(array, target, 0, array.length-1)
-	
+	return BSHelper(array, target, 0, array.length - 1)
 }
+
+function BSHelper (array, target, pointerLow, pointerHigh){
+
+	while (pointerLow <= pointerHigh){
+		let middle = Math.floor((pointerLow + pointerHigh)/2);
+		if (target === array[middle]){return middle;}
+		else if	(target < array[middle]){
+			pointerHigh = middle - 1;
+		}
+		else {
+			pointerLow = middle + 1;
+		}
+	}
+	if(pointerLow > pointerHigh){return -1;}
+}
+	
