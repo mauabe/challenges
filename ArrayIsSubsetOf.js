04f0d676a0b0a61e7d2758bf7c3be3ecc7f78298
Mauricio Feldman-Abe <6901736+mauabe@users.noreply.github.com>
2018-11-07 20:05:45 -0800

    Update ArrayIsSubsetOf
    using array prototype and storage object

--- ArrayIsSubsetOf.js
+++ ArrayIsSubsetOf.js
@@ -22,37 +22,6 @@ Array.prototype.isSubsetOf = function(sub) {
 
 
 
-/*
-Array isSubset
-Create a function that takes two arrays and determine whether the second array is a subset of the first array. Arrays will only contain primitive values.
-Examples
-Input	Output
-array:
-[ 1, 2, 3, 4 ]
-sub:
-[ 2, 3, 4 ]	true
-array:
-[ 2, 2, 2, 3, 4 ]
-sub:
-[ 2, 4, 3 ]	true
-array:
-[ 2, 3, 3, "a" ]
-sub:
-[ 3, 3, 3, "a" ]	false
-*/
-
-// function isSubsetOf (array, sub) {
-//   if(sub[0] === undefined){return true};
-//   for (let i = 0; i < sub.length; i++){
-//     if(array.indexOf(sub[i], 0) === -1){
-//       return false;}
-//     else {array[array.indexOf(sub[i])] = null};
-//     }
-//   isSubset(array, sub);
-//   return true;
-// }
-
-
 console.log(['merge','reset','reset'].isSubsetOf(['reset','merge','add','commit'])) // true
 console.log([ 1, 2, 3, 4 ].isSubsetOf([ 2, 3, 4 ]));
 console.log(['cat', 'dog', 'cow'].isSubsetOf(['dog', 'cow', 'fox', 'cat']));
