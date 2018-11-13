ea1550a6dcf02b288a458b6858ac01179012fba6
Mauricio Feldman-Abe <mauabe@gmx.com>
2018-11-13 09:41:36 -0800

    solved frequency using  object storage, map and sort

---
+++ CharacterFrequency.js
@@ -0,0 +1,42 @@
+/*
+Character Frequency
+Write a function that takes as its input a string and returns 
+an array of arrays as shown below sorted in descending order by 
+frequency and then by ascending order by character.
+
+Examples
+Input	Output
+string:
+"aaabbc"	[ [ "a", 3 ], [ "b", 2 ], [ "c", 1 ] ]
+string:
+"mississippi"	[ [ "i", 4 ], [ "s", 4 ], [ "p", 2 ], [ "m", 1 ] ]
+string:
+""	[ ]
+*/
+
+
+function characterFrequency (string) {
+  let storage = {};
+  let result = [];
+
+  for(let char of string){
+    if (storage[char]){
+      storage[char]++;
+    } else {
+      storage[char] = 1;
+    }
+    char++;
+  }
+  //console.log(storage)
+  
+  for(let key in storage){
+    result = Object.keys(storage).map(prop => ([prop, storage[prop]]))
+  }
+
+
+  //console.log(result)
+  result.sort((a, b) => (a[1] < b[1]) ? 1 : -1);
+  return result;
+  }
+
+console.log(characterFrequency ("mississippi"))
