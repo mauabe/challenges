cff3cf4fa006ff536d8ae636dbfa14faee025f84
Mauricio Feldman-Abe <6901736+mauabe@users.noreply.github.com>
2018-11-15 21:15:53 -0800

    plus minus

--- PlusMinus.js
+++ PlusMinus.js
@@ -1,8 +1,6 @@
 'use strict';
-
 process.stdin.resume();
 process.stdin.setEncoding('utf-8');
-
 let inputString = '';
 let currentLine = 0;
 
@@ -14,7 +12,6 @@ process.stdin.on('end', _ => {
     inputString = inputString.replace(/\s*$/, '')
         .split('\n')
         .map(str => str.replace(/\s*$/, ''));
-
     main();
 });
 
@@ -22,7 +19,6 @@ function readLine() {
     return inputString[currentLine++];
 }
 
-// Complete the plusMinus function below.
 function plusMinus(arr) {
 
 
@@ -30,8 +26,6 @@ function plusMinus(arr) {
 
 function main() {
     const n = parseInt(readLine(), 10);
-
     const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
-
     plusMinus(arr);
 }
