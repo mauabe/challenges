fa928ccbd067af311f63e4334cf3f26b98a9ec5f
Mauricio Feldman-Abe <6901736+mauabe@users.noreply.github.com>
2018-11-15 22:09:15 -0800

    plus minus

--- PlusMinus.js
+++ PlusMinus.js
@@ -20,9 +20,20 @@ function readLine() {
 }
 
 function plusMinus(arr) {
-
-
+  let p = 0;
+  let n = 0;
+  let z = 0;
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i] > 0){p++}
+    if (arr[i] < 0){n++}
+    if (arr[i] === 0){z++}
+  }
+  console.log ((p/arr.length).toFixed(6));
+  console.log((n/arr.length).toFixed(6));
+  console.log((z/arr.length).toFixed(6));
 }
+
+console.log(plusMinus([4, 3, 0, 0, 4, 1 ]))
 
 function main() {
     const n = parseInt(readLine(), 10);
