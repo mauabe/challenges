3a3d769dbf5ca1010f56a635d2267df5b52d06b3
Mauricio Feldman-Abe <6901736+mauabe@users.noreply.github.com>
2018-11-16 17:53:12 -0800

    fist commit

---
+++ SmallestDifference.js
@@ -0,0 +1,12 @@
+~function smallestDifference(arrayOne, arrayTwo) {
+  // iterate arrayOne
+	// iterate arrayTwo
+	
+	// find pair that adds to zero
+	// if there is none, increase to one/ negative one
+	//recurse
+	
+	const result = [];
+	
+}
+	
