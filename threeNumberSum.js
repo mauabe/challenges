3922dc0989ac209fc912b8232fe38d1491e6dd30
Mauricio Feldman-Abe <6901736+mauabe@users.noreply.github.com>
2018-11-26 11:18:29 -0800

    first commit

---
+++ threeNumberSum.js
@@ -0,0 +1,13 @@
+function threeNumberSum(array, targetSum) {
+  // sort array
+  array.sort((a, b) => (a - b))
+  // loop 
+  
+  // sum: pointer before, current number and final number.
+	// sum > target, move current left
+	// sum < target, move current right
+	// sum === target, push tripplet
+	// if current === final, final--
+	
+	
+}
