863336255cf4b27cf87022056e398c4295bd1f24
Mauricio Feldman-Abe <6901736+mauabe@users.noreply.github.com>
2018-11-28 07:17:26 -0800

    solves 3Sum with pointers, just like in C

--- threeNumberSum.js
+++ threeNumberSum.js
@@ -1,13 +1,23 @@
 function threeNumberSum(array, targetSum) {
-  // sort array
   array.sort((a, b) => (a - b))
-  // loop 
-  
-  // sum: pointer before, current number and final number.
-	// sum > target, move current left
-	// sum < target, move current right
-	// sum === target, push tripplet
-	// if current === final, final--
-	
-	
+	const result = [];
+  for(let i = 0; i < array.length - 2; i++){
+		let current = i
+    let left = i + 1;
+    let right = array.length - 1;
+    
+		while (left < right){
+      let sum = array[current] + array[left] + array[right];
+			if (sum === targetSum){
+      	result.push([array[current], array[left], array[right]])
+				left += 1;
+				right -= 1;
+    	}
+			else if (sum < targetSum){left += 1;}
+			else if (sum > targetSum){right -= 1;}
+		}
+  }
+	return result;
 }
+
+console.log (threeNumberSum([-12, 3, 1, 2, 6, -6, 5, -8 ], 0)) // [[-8, 2, 6], [-8, 3, 5],[-6, 1, 5]]
