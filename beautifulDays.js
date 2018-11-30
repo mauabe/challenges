1fdf10c1d3cd7fd9dac26c1c700352844535163a
Mauricio Feldman-Abe <mauabe@gmx.com>
2018-11-30 11:46:59 -0800

    stringify, reverse, parseInt and module solution

--- beautifulDays.js
+++ beautifulDays.js
@@ -5,20 +5,29 @@ process.stdin.setEncoding('utf-8');
 let inputString = '';
 let currentLine = 0;
 process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
+  inputString += inputStdin;
 });
 process.stdin.on('end', _ => {
-    inputString = inputString.replace(/\s*$/, '')
-        .split('\n')
-        .map(str => str.replace(/\s*$/, ''));
-    main();
+  inputString = inputString.replace(/\s*$/, '')
+    .split('\n')
+    .map(str => str.replace(/\s*$/, ''));
+  main();
 });
 function readLine() {return inputString[currentLine++];}
 
 function beautifulDays(i, j, k) {
-
-
+  const storage = {};
+  for(let x = i; x <= j; x++){
+    let y = JSON.stringify(x).split("").reverse().join("");
+    //console.log(y);
+    if ((x - parseInt(y)) % k ===  0 ){storage[x] = true}
+    console.log(storage);
+  }
+  return Object.keys(storage).length;
 }
+
+console.log(beautifulDays(20, 23, 6))
+
 
 function main() {
     const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
